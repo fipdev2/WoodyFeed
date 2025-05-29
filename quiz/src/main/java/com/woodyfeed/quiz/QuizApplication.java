dc3b46b3b1372610937d1ff6292cb4ee2bc28d2e
Filipe Magalhães <filipemagalha3s@gmail.com>
2025-05-29 11:52:15 -0300

    refactor: remove HelloWorldController from QuizApplication

--- quiz/src/main/java/com/woodyfeed/quiz/QuizApplication.java
+++ quiz/src/main/java/com/woodyfeed/quiz/QuizApplication.java
@@ -2,19 +2,9 @@ package com.woodyfeed.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class QuizApplication {
-	@RestController
-	public class HelloWorldController {
-		@GetMapping("/hello")
-		public String helloWorld() {
-			return "Hello, World!";
-		}
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
